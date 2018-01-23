af3dac7b2961d9422004b248068c13b6a27df8ce
Kyle Chambers <kylejchambers@gmail.com>
2018-01-22 21:04:04 -0800

    almost there...

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,18 @@
 var RetiredForagerBee = function() {
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  var response = 'I am too old, let me play cards instead';
+  return response;
+}
+
+RetiredForagerBee.prototype.gamble = function (value) {
+  this.treasureChest.push(value);
+}
